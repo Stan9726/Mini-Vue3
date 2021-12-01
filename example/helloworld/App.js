2a4b636f20bfd3669bfdc3467ff6184191d7c3fe
Stan <Stan9726@users.noreply.github.com>
2021-12-01 23:20:36 +0800

    实现 Element 初始化的主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -4,12 +4,16 @@ import { h } from '../../lib/mini-vue3.esm.js'
 export const App = {
 	// render 函数
 	render() {
-    // 在 render 函数中能够获取 setup 返回对象的属性
-		return h('div', 'hello, ' + this.msg)
+		// 在 render 函数中能够获取 setup 返回对象的属性
+		// return h('div', 'hello, ' + this.msg)
+		return h('div', { id: 'root', class: 'root' }, [
+			h('p', { id: 'p1', class: 'p1' }, 'hello, mini-vue3'),
+			h('p', { id: 'p2', class: 'p2' }, 'this is mini-vue3')
+		])
 	},
 	// composition API
 	setup() {
-    // 返回一个对象
+		// 返回一个对象
 		return {
 			name: 'mini-vue3'
 		}
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,8 +1,7 @@
+import { isObject } from './../shared/index'
 import { createComponentInstance, setupComponent } from './component'
 
-/**
- * 用于调用 patch 方法（为了方便后续进行递归地处理）
- */
+// 用于调用 patch 方法（为了方便后续进行递归地处理）
 export function render(vnode, container) {
 	patch(vnode, container)
 }
@@ -10,10 +9,55 @@ export function render(vnode, container) {
 // 用于处理组件对应的 VNode
 function patch(vnode, container) {
 	// 根据 VNode 类型的不同调用不同的函数
+	// 通过 VNode 的 type 属性值的类型来判断 VNode 类型
+	// 若 type 属性值的类型是 string，则 VNode 类型是 Element
+	if (typeof vnode.type === 'string') {
+		processElement(vnode, container)
+	}
+	// 若 type 属性值的类型是 object，则 VNode 类型是 Component
+	else if (isObject(vnode.type)) {
+		processComponent(vnode, container)
+	}
+}
 
-	processComponent(vnode, container)
+// 用于处理 Element
+function processElement(vnode, container) {
+	mountElement(vnode, container)
+}
 
-	// TODO: 调用 processElement
+// 用于进行 Element 的初始化
+function mountElement(vnode, container) {
+	// 根据 Element 对应 VNode 的 type 属性的值创建 DOM 元素并赋值给变量 el
+	const el = document.createElement(vnode.type)
+
+	// 通过解构赋值获取 Element 对应 VNode 的 props 和 children 属性的值
+	const { props, children } = vnode
+
+	// 遍历 props 属性的值，通过 Element.setAttribute() 将其中的属性添加到 el 上
+	// 其中 key 作为 el 的属性名，value 作为 属性值
+	for (const key in props) {
+		const val = props[key]
+		el.setAttribute(key, val)
+	}
+
+	// 若 children 属性值的类型是 string，则将其赋值给 el 的 textContent 属性
+	if (typeof children === 'string') {
+		el.textContent = children
+	}
+	// 若 children 属性值的类型是 Array，则调用 mountChildren 函数
+	else if (Array.isArray(children)) {
+		mountChildren(children, el)
+	}
+
+	// 通过 Element.append() 将 el 添加到根容器/其父元素中
+	container.append(el)
+}
+
+// 用于遍历 children 属性的值，对其中每个 VNode 调用 patch 方法进行处理
+function mountChildren(children, container) {
+	children.forEach(child => {
+		patch(child, container)
+	})
 }
 
 // 用于处理 Component

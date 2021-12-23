6367683e8533da885a50e541ec393b6e9500b608
Stan <Stan9726@users.noreply.github.com>
2021-12-23 17:11:20 +0800

    format code

--- rollup.config.js
+++ rollup.config.js
@@ -3,21 +3,21 @@ import typescript from '@rollup/plugin-typescript'
 
 // 可以直接使用 ESM
 export default {
-	// 库的入口文件
-	input: './src/index.ts',
-	// 打包完成后的输出
-	output: [
-		// CommonJS
-		{
-			format: 'cjs',
-			file: pkg.main
-		},
-		// ESM
-		{
-			format: 'es',
-			file: pkg.module
-		}
-	],
+  // 库的入口文件
+  input: './src/index.ts',
+  // 打包完成后的输出
+  output: [
+    // CommonJS
+    {
+      format: 'cjs',
+      file: pkg.main
+    },
+    // ESM
+    {
+      format: 'es',
+      file: pkg.module
+    }
+  ],
   // 配置插件 @rollup/plugin-typescript
-	plugins: [typescript()]
+  plugins: [typescript()]
 }

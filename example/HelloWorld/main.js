5d2e535f06df9a3a370a192eb4cfec181a581c5f
Stan <Stan9726@users.noreply.github.com>
2022-01-02 21:29:33 +0800

    rename example folders

--- example/HelloWorld/main.js
+++ example/HelloWorld/main.js
@@ -1,5 +1,5 @@
 import { createApp } from '../../lib/mini-vue3.esm.js'
-import { App } from './App.js'
+import { App } from './App.js.js'
 
 const rootContainer = document.querySelector('#app')
 const app = createApp(App)
